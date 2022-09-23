2ba25db40bbac87db2a6411d98e354396d1c01f4
jsonSM99 <jeisson.santiesteban@mercadolibre.com.co>
2022-09-23 12:12:40 -0500

    Solucion practica manhana 23 septiembre 2022

---
+++ go_bases/clase_23_sep_1/ejercicios/ejercicio4.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	var apellido string = "Gomez" // ok
+	var edad int = 35 // string a int
+	boolean := false; // string a boolean
+	var sueldo float64 = 45857.90 // string a float64
+	var nombre string = "Julián" // ok
+}
