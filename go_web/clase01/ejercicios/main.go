d6d03e73983f00413974bb76d487df294bb4d9aa
jsonSM99 <jeisson.santiesteban@mercadolibre.com.co>
2022-10-04 08:06:56 -0500

    go-web --> clase 03 octubre y ejercicios

--- go_web/clase01/ejercicios/main.go
+++ go_web/clase01/ejercicios/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
+	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -39,11 +41,72 @@ func parseTransactions(path string) (data []Transaction) {
 	return data
 }
 
+func filterData(ctx *gin.Context, data []Transaction) (filtered []Transaction) {
+	id := ctx.Query("id")
+	codigoTransaccion := ctx.Query("codigoTransaccion")
+	moneda := ctx.Query("moneda")
+	monto := ctx.Query("monto")
+	emisor := ctx.Query("emisor")
+	receptor := ctx.Query("receptor")
+	fecha := ctx.Query("fecha")
+	if id == "" &&
+		codigoTransaccion == "" &&
+		moneda == "" &&
+		monto == "" &&
+		emisor == "" &&
+		receptor == "" &&
+		fecha == "" {
+		return data
+	}
+	for _, t := range data {
+		if _id, err := strconv.Atoi(id); err != nil && _id == t.Id {
+			filtered = append(filtered, t)
+			continue
+		}
+		if _monto, err := strconv.Atoi(monto); err != nil && _monto == int(t.Monto) {
+			filtered = append(filtered, t)
+			continue
+		}
+		if codigoTransaccion == t.CodigoTransaccion ||
+			moneda == t.Moneda ||
+			emisor == t.Emisor ||
+			receptor == t.Receptor ||
+			fecha == t.Fecha {
+			filtered = append(filtered, t)
+			continue
+		}
+	}
+	return
+}
+
 func GetAll(ctx *gin.Context) {
 	data := parseTransactions("transacciones.json")
 
+	filteredData := filterData(ctx, data)
+
 	ctx.JSON(200, gin.H{
-		"accepted": data,
+		"accepted": filteredData,
+	})
+}
+
+func GetOne(ctx *gin.Context) {
+	data := parseTransactions("transacciones.json")
+
+	id := ctx.Param("id")
+	if id == "" {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"message": id,
+		})
+		return
+	}
+	var transaction Transaction
+	for _, t := range data {
+		if _id, err := strconv.Atoi(id); err == nil && t.Id == _id {
+			transaction = t
+		}
+	}
+	ctx.JSON(200, gin.H{
+		"accepted": transaction,
 	})
 }
 
@@ -53,6 +116,7 @@ func main() {
 
 	router.GET("/greetings", MessageHandler)
 	router.GET("/transactions", GetAll)
+	router.GET("/transactions/:id", GetOne)
 
 	router.Run()
 }
